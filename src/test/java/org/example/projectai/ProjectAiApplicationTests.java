42f5ab83fb2e4b3336a610f4a646334bb32a2f56
Kapil Dev Saini <saini17102001@gmail.com>
2025-05-10 10:22:40 +0530

    Fresh Project

---
+++ src/test/java/org/example/projectai/ProjectAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
